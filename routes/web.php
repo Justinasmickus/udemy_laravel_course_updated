3aefb59afc7eb19078f22ad0e601daa5b47e59ad
Justinas Mickus <mickusjustinas1@gmail.com>
2022-09-07 19:19:20 +0200

    routes

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,40 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $html = "
+    <h1>Contact app</h1>
+    <div>
+        <a href='" . route('contacts.index') . "'>All contacts</a>
+        <a href='" . route('contacts.create') . "'>Add contact</a>
+        <a href='" . route('contacts.show', 1) . "'>Show contact</a>
+    </div>
+    ";
+    return $html;
 });
+
+Route::prefix('admin')->group(function (){
+    Route::get('/contacts', function(){
+        return "<h1>All contacts</h1>";
+    })->name('contacts.index');
+    
+    Route::get('/contacts/create', function(){
+        return "<h1>Add new contact</h1>";
+    })->name('contacts.create');
+    
+    Route::get('/contacts/{id}', function($id){
+        return "Contact" . $id;
+    })->whereNumber('id')->name('contacts.show'); // numeric values only
+});
+
+
+Route::fallback(function (){
+    return "<h1>Sorry, this page does not exist :( </h1>";
+});
+
+// Route::get('/companies/{name?}', function($name = null){
+//     if ($name) {
+//         return "Company " . $name;
+//     } else {
+//         return "All companies";
+//     }
+// })->whereAlphaNumeric('name'); // alphabetical symbols and numbers
