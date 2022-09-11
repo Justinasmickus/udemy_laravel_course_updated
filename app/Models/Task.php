dbb27b61aa92977800540b2f12debee909e66334
Justinas Mickus <mickusjustinas1@gmail.com>
2022-09-11 21:15:43 +0200

    factory relationships

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,5 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+
+    public function contacts()
+    {
+        return $this->belongsTo(Contact::class);
+    }
     
 }
