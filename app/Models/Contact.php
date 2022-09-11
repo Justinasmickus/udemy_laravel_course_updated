f1c309fbccde5d4c00c6624c8e1c8cb3ceaf3956
Justinas Mickus <mickusjustinas1@gmail.com>
2022-09-11 17:31:10 +0200

    seeding the contacts table

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -5,10 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Company extends Model
+class Contact extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name','email','address','website'];
-    
+    protected $fillable = ['first_name', 'last_name', 'email','phone','address'];
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\ContactSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +15,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(CompanySeeder::class);
+        $this->call([
+            CompanySeeder::class,
+            ContactSeeder::class
+        ]);
     }
 }
