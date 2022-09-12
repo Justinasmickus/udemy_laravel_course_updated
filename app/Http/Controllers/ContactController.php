be0cf63a7aec3f0f00014d280acce8ac15393266
Justinas Mickus <mickusjustinas1@gmail.com>
2022-09-12 17:24:34 +0200

    delete http method

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -92,5 +92,13 @@ class ContactController extends Controller
         $contact->update($request->all());
         return redirect()->route('contacts.index')->with('message', 'Contact has been updated successfully');
     }
+
+    public function destroy($id)
+    {
+        
+        $contact = Contact::findOrFail($id);
+        $contact->delete();
+        return redirect()->route('contacts.index')->with('message', 'Contact has been removed successfully');
+    }
     
 }
