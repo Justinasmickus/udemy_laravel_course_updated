76bbcc21b79f525c09416d211e3122f9b3375d6b
Justinas Mickus <mickusjustinas1@gmail.com>
2022-09-12 16:48:00 +0200

    handling http responses

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -65,7 +65,8 @@ class ContactController extends Controller
             'address' => 'nullable',
             'company_id' => 'required|exists:companies,id'
         ]);
-        dd($request->all());
+        Contact::create($request->all());
+        return redirect()->route('contacts.index')->with('message', 'Contact has been added successfully');
     }
     
 }
